1684604f62cb005fb526cfa7e3d35323aeed9648
lukewtait <59702793+lukewtait@users.noreply.github.com>
2020-01-10 14:45:50 +0000

    Update read_atlases.m

--- reduced_atlas/read_atlases.m
+++ reduced_atlas/read_atlases.m
@@ -3,7 +3,7 @@ clear, clc, close all
 
 % Read volumetric atlas
 volHCP = niftiread('vol_HCP250.nii') ; % atlas
-T = readmatrix('vol_transform.txt') ; % transform
+T = dlmread('vol_transform.txt') ; % transform
 
 % Plot volumetric atlas
 grey = find(volHCP>0) ; % voxels which are labelled
@@ -12,14 +12,14 @@ P = ones(4,length(grey)) ; % initialize coordinates
 P = (T*P)' ; % do the affine transformation
 figure(1), clf 
 scatter3(P(:,1),P(:,2),P(:,3),5,volHCP(grey),'filled') % plot the voxels, with color corresponding to atlas label
-cmap = readmatrix('ROI_colors.txt') ; % get original colours from atlas printed in Glasser et al for reproducibility
+cmap = dlmread('ROI_colors.txt') ; % get original colours from atlas printed in Glasser et al for reproducibility
 colormap(cmap)
 
 % Read surface atlas
 surf = struct ; 
-surf.vertices = readmatrix('surf_vertices.txt') ; 
-surf.faces = readmatrix('surf_faces.txt') ; 
-surfHCP = readmatrix('surf_HCP250.txt') ; 
+surf.vertices = dlmread('surf_vertices.txt') ; 
+surf.faces = dlmread('surf_faces.txt') ; 
+surfHCP = dlmread('surf_HCP250.txt') ; 
 figure(2), clf
 patch(surf,'facecolor','flat','facevertexCdata',surfHCP,'linestyle','none')
 camlight headlight; lighting gouraud; material dull ; 
